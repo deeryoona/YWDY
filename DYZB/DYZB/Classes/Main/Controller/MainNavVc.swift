f424f0072d3ff508a03c0d80211fcd1821b930a1
yanwei <764665007@qq.com>
2016-09-15 10:27:00 +0800

    项目初始化呢

---
+++ DYZB/DYZB/Classes/Main/Controller/MainNavVc.swift
@@ -0,0 +1,35 @@
+//
+//  MainNavVc.swift
+//  DYZB
+//
+//  Created by jhyw12 on 16/9/15.
+//  Copyright © 2016年 jhyw12. All rights reserved.
+//
+
+import UIKit
+
+class MainNavVc: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
